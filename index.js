af9f83f7b1ca5738ff943a528321643de1e83e29
Inbal Mishory <mishory@gmail.com>
2024-09-22 09:20:43 +0300

    Finalize js code

--- index.js
+++ index.js
@@ -6,94 +6,55 @@ const mainImage = document.querySelector(".main-image");
 const mainImageTitle = document.querySelector(".main-image-title");
 const carouselItemsImages = document.querySelectorAll('.carousel-item-image');
 
-
 const totalItems = carouselItemsImages.length;
-let currentIndex = 0;
-let prevIndex;
-let nextIndex;
-
+let currentIndex = 2;
 const imageWidth = 235;
 
-// Add event listeners to carousel items
-carouselItemsImages.forEach(image => {
-  image.addEventListener("click", () => {
-    const imageIndex = parseFloat(image.id);
-
-    changeMainImage(imageIndex);
-
-    if (imageIndex > currentIndex) {
-      carouselGoBack(imageIndex); 
-    } else {
-      carouselGoForward(imageIndex);
-    }
-  })
-})
 
 // Move Carousel items right
 arrowLeft.addEventListener("click", () => {
-  carouselGoForward();
+  moveCarousel(-1);
 });
 
 // Move Carousel items left
 arrowRight.addEventListener("click", () => {
-  carouselGoBack();
+  moveCarousel(1);
 });
 
 function changeMainImage(index) {
   mainImage.classList.add("fadeOutTransition");
-
   setTimeout(() => {
     mainImage.src = carouselItemsImages[index].src;
     mainImageTitle.innerHTML = carouselItemsImages[index].alt;
     mainImage.classList.remove("fadeOutTransition");
-  }, 500)
+  }, 500);
 }
 
-function carouselGoForward(index = currentIndex) {
-  prevIndex = index;
-  currentIndex = (index - 1 + totalItems) % totalItems;
-  const currentCarouselItem = carouselItemsImages[currentIndex];
-  const prevCarouselItem = carouselItemsImages[prevIndex];
+function moveCarousel(direction) {
+  const prevIndex = currentIndex;
+  currentIndex = (currentIndex + direction + totalItems) % totalItems;
+  const nextIndex = (currentIndex + (direction * 3) + totalItems) % totalItems;
+  const nextCarouselItem = carouselItemsImages[nextIndex];
 
-  // Carousel image shift
-  carousel.style.transform = `translateX(-${imageWidth}px)`;
-  currentCarouselItem.classList.add("fading-transition");
-
-  carousel.insertBefore(currentCarouselItem, carousel.firstChild);
-
-  setTimeout(() => {
-    carousel.style.transform = "";
-    carousel.classList.add("sliding-transition");
-    currentCarouselItem.classList.add('active');
-    // Load new main image and text
-    changeMainImage(currentIndex);
-  }, 10);
-
-  setTimeout(() => {
-    carousel.classList.remove("sliding-transition");
-    currentCarouselItem.classList.remove("fading-transition");
-    prevCarouselItem.classList.remove('active');
-  }, 490);
-}
-
-function carouselGoBack(index = currentIndex) {
-  prevIndex = index;
-  currentIndex = (index + 1) % totalItems;
-  const currentCarouselItem = carouselItemsImages[currentIndex];
-  const prevCarouselItem = carouselItemsImages[prevIndex];
-
-  // Carousel image shift
+  carousel.style.transform = `translateX(${direction * -imageWidth}px)`;
   carousel.classList.add("sliding-transition");
 
-  changeMainImage(currentIndex);
-
-  carousel.style.transform = `translateX(-${imageWidth}px)`;
-
   setTimeout(() => {
-    currentCarouselItem.classList.add('active');
-    prevCarouselItem.classList.remove('active');
-    carousel.appendChild(prevCarouselItem);
-    carousel.classList.remove("sliding-transition");
     carousel.style.transform = "";
+    carousel.classList.remove("sliding-transition");
+    setActiveImage(currentIndex);
+    changeMainImage(currentIndex);
+    if (direction === -1) {
+      carousel.insertBefore(nextCarouselItem, carousel.firstChild);
+    } else {
+      carousel.appendChild(nextCarouselItem);
+    }
   }, 500);
+}
+
+function setActiveImage(index) {
+  carouselItemsImages.forEach(image => {
+    image.classList.remove('active');
+  });
+  carouselItemsImages[index].classList.add('active');
 }
